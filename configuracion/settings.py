ac2f208b6617c44c1f64feaa3a4f09a8e07f2153
roberth987 <roberthdarioamundaray@gmail.com>
2026-02-28 15:22:04 -0400

    Update settings.py

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -42,9 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'inventario',
-    'tiendas',
-    'cuentas'
+    'inventario'
 ]
 
 MIDDLEWARE = [
@@ -135,4 +133,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
